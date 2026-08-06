8cfc432881ee927f27d7f9ba87e5897634f8ae92
Hitesh Kasture <hiteshkasture8@gmail.com>
2026-08-06 23:00:55 +0530

    Add 3D aircraft showcase page at /aircrafts with Three.js and GSAP ScrollTrigger

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,7 +4,7 @@ import { PRODUCTS } from '../data/products';
 export const GET: APIRoute = async () => {
   const baseUrl = 'https://aks-aeromodels.com';
   
-  const staticPages = ['', '/shop', '/about', '/contact', '/privacy', '/terms', '/pricing-policy', '/blog'];
+  const staticPages = ['', '/aircrafts', '/shop', '/about', '/contact', '/privacy', '/terms', '/pricing-policy', '/blog'];
   
   const productUrls = PRODUCTS.map(p => `/product/${p.id}`);
   const blogUrls = [
